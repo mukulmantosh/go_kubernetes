2d7551a894922bc64e7545818b0247b10caf5f1d
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2024-10-21 14:42:07 +0530

    Updated README.md

--- controller.go
+++ controller.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func (s *MuxServer) addUser(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +18,58 @@ func (s *MuxServer) addUser(w http.ResponseWriter, r *http.Request) {
 
 	s.db.Create(&user)
 	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
+}
+
+func (s *MuxServer) listUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	s.db.Find(&users)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func (s *MuxServer) updateUser(w http.ResponseWriter, r *http.Request) {
+	var userData UserParam
+	err := json.NewDecoder(r.Body).Decode(&userData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	userId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var user User
+	s.db.First(&user, userId)
+	user.Name = userData.Name
+	user.Email = userData.Email
+	user.Age = userData.Age
+
+	s.db.Save(&user)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "User updated successfully",
+	})
+
+	return
+
+}
+
+func (s *MuxServer) deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	var user User
+	s.db.First(&user, userId)
+	s.db.Delete(&user)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "User deleted successfully",
+	})
+	return
 }
--- routes.go
+++ routes.go
@@ -2,5 +2,8 @@ package main
 
 func (s *MuxServer) routes() {
 	s.gorilla.HandleFunc("/user", s.addUser).Methods("POST")
+	s.gorilla.HandleFunc("/users", s.listUsers).Methods("GET")
+	s.gorilla.HandleFunc("/user/{id}", s.updateUser).Methods("PUT")
+	s.gorilla.HandleFunc("/user/{id}", s.deleteUser).Methods("DELETE")
 
 }
